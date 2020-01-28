2d8cb19dc8561c4ba60719750be26ac141576b3a
Burkard <burkarbr@LAPTOP-BURKARBR.asddev.reyrey.com>
2020-01-28 17:52:46 -0500

    passing data and callback

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -6,21 +6,17 @@ const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
 
-    //const [blogPosts, setBlogPosts] = useState([]);
+    const [blogPosts, setBlogPosts] = useState([{ title: 'Blog Post#1' },
+    { title: 'Blog Post#2' },
+    { title: 'Blog Post#3' }]);
 
-    const blogPosts = {
-        data: [
-            { title: 'Blog Post#1' },
-            { title: 'Blog Post#2' },
-            { title: 'Blog Post#3' }
-        ],
-        addBlogPost: () => {
-            return 'addPost';
-        }
+    const addBlogPost = () => {
+        setBlogPosts([...blogPosts,{ title: `Blog Post #${blogPosts.length + 1}` }]);
     }
 
+
     return (
-        <BlogContext.Provider value={blogPosts}>
+        <BlogContext.Provider value={{data: blogPosts, addBlogPost: addBlogPost}}>
             {children}
         </BlogContext.Provider>
     )
