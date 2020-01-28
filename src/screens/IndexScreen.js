30923394b02e18db808a16ef79e803db43ecb999
Burkard <burkarbr@LAPTOP-BURKARBR.asddev.reyrey.com>
2020-01-28 17:16:38 -0500

    wrap navigator

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {View, Text, StyleSheet } from 'react-native';
 
 const IndexScreen = () => {
-    return (<View>
+    return (
+    <View>
         <Text>Index Screen</Text>
-    </View>)
+    </View>
+    )
 }
 const styles = StyleSheet.create({})
 
