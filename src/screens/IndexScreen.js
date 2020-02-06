8c7728b50801c5145f3615ced50b41ad43056aff
Burkard <burkarbr@LAPTOP-BURKARBR.asddev.reyrey.com>
2020-02-06 15:46:52 -0500

    Show Screen

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -2,7 +2,8 @@ import React, { useContext } from 'react';
 import {View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context as BlogContext } from '../context/BlogContext';
 
-const IndexScreen = () => {
+const IndexScreen = ( { navigation } ) => {
+    
     const { state, addBlogPost, deleteBlogPost } = useContext(BlogContext);
 
     return (
@@ -14,14 +15,16 @@ const IndexScreen = () => {
             keyExtractor={(state)=>state.id.toString()}
             renderItem={({item}) => {
                 return (
-                    <View style={styles.row}>
-                        <Text style={styles.title}>{item.title} - {item.id}</Text>
-                        <TouchableOpacity onPress={()=>deleteBlogPost(item.id)}>
-                            <Text style={styles.icon}>
-                                &#9747;
-                            </Text>
-                        </TouchableOpacity>
-                    </View>
+                    <TouchableOpacity onPress={ () => navigation.navigate('Show', {id: item.id})}>
+                        <View style={styles.row}>
+                            <Text style={styles.title}>{item.title} - {item.id}</Text>
+                            <TouchableOpacity onPress={()=>deleteBlogPost(item.id)}>
+                                <Text style={styles.icon}>
+                                    &#9747;
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </TouchableOpacity>
                 )
             }}
         />
