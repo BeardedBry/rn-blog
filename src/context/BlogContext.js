8127cee18d22d777c2c5478afb433e671caae98d
Burkard <burkarbr@LAPTOP-BURKARBR.asddev.reyrey.com>
2020-02-06 14:53:29 -0500

    remove blog post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -6,7 +6,9 @@ const blogReducer = (state, action) => {
     
     switch (action.type) {
         case 'add_blogpost':
-            return [...state, { title: `Blog Post #${state.length + 1}` }];
+            return [...state, {id: Math.floor(Math.random() * 99999), title: `Blog Post #${state.length + 1}` }];
+        case 'delete_blogpost':
+            return state.filter((blogPost) => blogPost.id !== action.payload);
         default:
             return state;
     }
@@ -18,25 +20,16 @@ const addBlogPost = (dispatch) => {
     };
 };
 
+const deleteBlogPost = (dispatch) => {
+    return (id) => {
+        console.log(id);
+        dispatch({ type: 'delete_blogpost', payload: id});
+    };
+};
+
+
 export const { Context, Provider } = createDataContext(
     blogReducer,
-    { addBlogPost },
-    [{title: 'test'}]
+    { addBlogPost, deleteBlogPost },
+    []
 );
-
-// children is a react feature, not context specific.
-// export const BlogProvider = ({ children }) => {
-
-    // const [blogPosts, dispatch] = useReducer(blogReducer, [] );
-
-    // const addBlogPost = () => {
-    //     dispatch({type: 'add_blogpost'});
-    // };
-    
-   
-    // return (
-    //     <BlogContext.Provider value={{data: blogPosts, addBlogPost}}>
-    //         {children}
-    //     </BlogContext.Provider>
-    // )
-
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,32 +1,48 @@
 import React, { useContext } from 'react';
-import {View, Text, StyleSheet, FlatList, Button } from 'react-native';
-import { Context } from '../context/BlogContext';
-
+import {View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
+import { Context as BlogContext } from '../context/BlogContext';
 
 const IndexScreen = () => {
-
-    const { state, boundActions } = useContext(Context);
-
-    //console.log(actions);
+    const { state, addBlogPost, deleteBlogPost } = useContext(BlogContext);
 
     return (
     <View>
         <Text>Index Screen</Text>
-        <Button title="Add Post" onPress={boundActions.addBlogPost} />
+        <Button title="Add Post" onPress={addBlogPost} />
         <FlatList 
             data={state}
-            keyExtractor={(state)=>state.title}
+            keyExtractor={(state)=>state.id.toString()}
             renderItem={({item}) => {
                 return (
-                    <>
-                        <Text>{item.title}</Text>
-                    </>
+                    <View style={styles.row}>
+                        <Text style={styles.title}>{item.title} - {item.id}</Text>
+                        <TouchableOpacity onPress={()=>deleteBlogPost(item.id)}>
+                            <Text style={styles.icon}>
+                                &#9747;
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
                 )
             }}
         />
     </View>
     )
 }
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 15,
+        paddingHorizontal: 10,
+        borderTopWidth: 1,
+        borderColor: 'gray'
+    },
+    title: {
+        fontSize: 18,
+    },
+    icon: {
+        fontSize: 18,
+    }
+})
 
 export default IndexScreen;
